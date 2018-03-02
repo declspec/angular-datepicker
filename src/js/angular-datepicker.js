1d337e2960d3d32a98fd9f574fcd461ea13d03fe
Jason Larke <jason.larke@curtin.edu.au>
2018-03-02 15:27:04 +0800

    Removed isMobile detection

--- src/js/angular-datepicker.js
+++ src/js/angular-datepicker.js
@@ -4,20 +4,7 @@
   'use strict';
 
   var A_DAY_IN_MILLISECONDS = 86400000
-    , isMobile = (function isMobile() {
-
-      if (navigator.userAgent &&
-        (navigator.userAgent.match(/Android/i) ||
-        navigator.userAgent.match(/webOS/i) ||
-        navigator.userAgent.match(/iPhone/i) ||
-        navigator.userAgent.match(/iPad/i) ||
-        navigator.userAgent.match(/iPod/i) ||
-        navigator.userAgent.match(/BlackBerry/i) ||
-        navigator.userAgent.match(/Windows Phone/i))) {
-
-        return true;
-      }
-    }())
+    , isMobile = false
     , generateMonthAndYearHeader = function generateMonthAndYearHeader(prevButton, nextButton, preventMobile) {
 
       if (preventMobile) {
